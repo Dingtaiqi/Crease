b81ebe21b103820145042064e3c3313618785707
Dingtaiqi <107164943+Dingtaiqi@users.noreply.github.com>
2022-12-24 19:20:03 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+
+class click {
+public:
+	char ac;          //监视键盘输入，目前纯单压
+	struct juge;      //四个判定点于判定位的note，目前先写一个
+	int getSize();    //逻辑判断是否到达判定点
+	void accept ();   //获取键盘监视
+	int search();     //在结构体中寻找符合条件的note
+};
+int click::getSize() {
+	switch (ac == 'D') {          //                           需要添加范围判断
+		search();
+
+		return 0;
+	}
+};
+struct click::juge {
+	int val;
+	int x;
+	int y;
+	int next;
+	bool need;
+}muisc[11451],D;
+int click::search() {
+	int i = 0;
+	int bef = 11451;
+	if (muisc[i].y == D.y) {
+		muisc[i].need = false;    //链表删除节点实现note消失
+		muisc[bef].next = muisc[i].next;
+	};
+	 else  bef = i;
+	i = muisc[i].next;
+};
